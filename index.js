5cdadf0b6efae00814c581a0308e955b49107238
sergey <110@mobilon.ru>
2022-04-07 00:48:03 +0700

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Manager {
     this.counter = 0;
   }
   
-  ready (id) {
+  async ready (id) {
     const ready = this.queueReady.find(c => c.id === id );
     if (ready) {
       if (this.queueReady.find(c => c.counter < ready.counter)) {
@@ -22,27 +22,26 @@ class Manager {
     }
 
     this.counter++;
+    const timeoutNotReady = (this.queueNotReady.length) * 15 * 1000;
 
-    const timeout = (this.queueNotReady.length + 1) * 15 * 1000;
     const notReadyObj = {
       id,
-      callback: () => {
-        setTimeout(() => {
-          this.queueNotReady = this.queueNotReady.filter(c => c.id !== id);
-          const readyObj = {
-            id,
-            counter: this.counter,
-            callback: setTimeout(() => {
-              this.queueReady = this.queueReady.filter(c => c.id !== id)
-            }, 15 * 1000),
-          };          
-          this.queueReady.push(readyObj);
-        }, timeout);
-      }
+      callback: (async () => {
+        await delay(timeoutNotReady);
+        this.queueNotReady = this.queueNotReady.filter(c => c.id !== id);
+        const readyObj = {
+          id,
+          counter: this.counter,
+          callback: (async () => {
+            const timeoutReady = (this.queueReady.length + 1) * 15 * 1000;
+            await delay(timeoutReady);
+            this.queueReady = this.queueReady.filter(c => c.id !== id);
+          })(),
+        };
+        this.queueReady.push(readyObj);        
+      })(),
     };
-    notReadyObj.callback();
     this.queueNotReady.push(notReadyObj);
-
     return false;
   }
 
@@ -62,14 +61,15 @@ const start = async () => {
   const r2 = '1213';
   const r3 = '1214';
 
-  console.log('start', r1, m.ready(r1),);
+  console.log('start');
+  console.log('start', r1, await m.ready(r1));
   await delay(2 * 1000);
-  console.log(r2, m.ready(r2));
+  console.log(r2, await m.ready(r2));
   await delay(3 * 1000);
-  console.log(r3, m.ready(r3));
+  console.log(r3, await m.ready(r3));
 
   for (const r of [...Array(100).keys()]) {
-    console.log(r, r1, m.ready(r1), r2, m.ready(r2), r3, m.ready(r3),);  
+    console.log(r, r1, await m.ready(r1), r2, await m.ready(r2), r3, await m.ready(r3),);  
     console.log(r, m.getNotReadyQueue().length, m.getReadyQueue().length);
     //console.log(r, m.getNotReadyQueue(), m.getReadyQueue());
     await delay(1 * 1000);
